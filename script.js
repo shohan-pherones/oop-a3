145f78a8ff7b1bd4246fc96a90ec11226d84a7c1
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-10-11 23:42:49 +0600

    oc

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ class Student {
     this.birthYear = birthYear;
   }
 
-  calcAge() {
+  calcbirthYear() {
     return 2022 - this.birthYear;
   }
 }
@@ -24,7 +24,7 @@ const student2 = new Student("Sadril Amin", 1996);
 // console.log(student1);
 // console.log(student2);
 
-console.log(student1.calcAge());
+console.log(student1.calcbirthYear());
 
 // console.log(student1.__proto__);
 
@@ -85,7 +85,7 @@ const testObj = new Test(1995);
 
 console.log(testObj.hasOwnProperty("birthYear")); */
 
-/////////////////////////////////////////////////
+/* /////////////////////////////////////////////////
 // Constructor function
 /////////////////////////////////////////////////////
 function Person(nickName, birthYear) {
@@ -93,7 +93,7 @@ function Person(nickName, birthYear) {
   this.birthYear = birthYear;
 
   // Never do
-  // this.calcAge = function () {
+  // this.calcbirthYear = function () {
   //   console.log(2022 - birthYear);
   // };
 }
@@ -113,12 +113,12 @@ console.log(fahim);
 
 console.log(sadril instanceof Person);
 
-Person.prototype.calcAge = function () {
+Person.prototype.calcbirthYear = function () {
   console.log(2022 - this.birthYear);
 };
 
-sadril.calcAge();
-nasim.calcAge();
+sadril.calcbirthYear();
+nasim.calcbirthYear();
 
 console.log(Person.prototype);
 
@@ -127,4 +127,126 @@ console.log(Person.prototype === Person.prototype);
 
 console.log(Person.prototype.isPrototypeOf(sadril));
 console.log(Person.prototype.isPrototypeOf(fahim));
-console.log(Person.prototype.isPrototypeOf(Person));
+console.log(Person.prototype.isPrototypeOf(Person)); */
+
+/* function User(firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // Never do this
+  // this.calcAge = function () {
+  //   console.log(2022 - this.birthYear);
+  // };
+}
+
+User.prototype.calcAge = function () {
+  console.log(2022 - this.birthYear);
+};
+
+const rahim = new User("Rahim", 1996);
+rahim.calcAge();
+console.log(rahim);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(Array.prototype);
+
+const arr = [5, 4, 2, 5, 4, 2, 1, 3, 2, 2, 4, 5];
+
+console.log(arr.unique());
+
+const h1 = document.querySelector("h1");
+console.dir(h1); */
+
+//////////////////////////////////////
+// ES6 Classes
+/////////////////////////////////////
+
+class Person {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hello, ${this.fullName}!`);
+  }
+
+  static couponCode() {
+    console.log(515151514444);
+  }
+}
+
+const kajol = new Person("Kajol Rekha", 1995);
+const sadril = new Person("SAdril", 1994);
+
+kajol.calcAge();
+console.log(kajol);
+kajol.greet();
+console.log(sadril);
+console.log(Person.prototype === kajol.__proto__);
+console.log(Person.prototype.constructor);
+
+Person.couponCode();
+// kajol.couponCode();
+// Person.calcAge();
+
+// Classes are not HOISTED
+// Classes are first-class functions
+// Classes always use 'use strict'
+
+/* class CinemaHall {
+  constructor(movieName, seatCount) {
+    this.movieName = movieName;
+    this.seatCount = seatCount;
+    this.audience = [];
+  }
+  // // setter
+  // set seatCount(seats) {
+  //   console.log(seats);
+  // }
+
+  // //getter
+  // get ticketPrice() {
+  //   return this.seatCount * 100;
+  // }
+
+  audienceCount(name) {
+    this.audience.push(name, this.movieName, this.seatCount);
+  }
+}
+
+const abdullah = new CinemaHall("Inception", 10);
+const sumaiya = new CinemaHall("Snowman", 5);
+const suma = new CinemaHall("Snowman", 50);
+
+console.log(abdullah.ticketPrice);
+console.log(sumaiya.ticketPrice);
+abdullah.audienceCount("Abdullah");
+console.log(abdullah);
+abdullah.seatCount; */
+
+///////////////////////////////////////////
+// Object.create()
+///////////////////////////////////////////
+
+const PersonProto = {
+  calcAge() {
+    console.log(2022 - this.birthYear);
+  },
+};
+
+const jessica = Object.create(PersonProto);
+
+jessica.fullName = "Jessica Jennie";
+jessica.birthYear = 1990;
+
+console.log(jessica);
+
+jessica.calcAge();
